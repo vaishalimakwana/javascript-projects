f1b800060c531e254773581cfbb13bb24826ca7d
vaishalimakwana <24860123+vaishalimakwana@users.noreply.github.com>
2025-02-01 23:01:25 +0000

    questions.js updated

--- Fundamentals/Basic/questions.js
+++ Fundamentals/Basic/questions.js
@@ -61,3 +61,86 @@ console.log('Arr2', arr2);
 console.log('Arr3', arr3);
 console.log("Arr 1: length=" + arr1.length + " last=" + arr1.slice(-1));
 console.log("Arr 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+
+// 7 
+function sum(x){
+	if(arguments.length == 2){
+	return arguments[0] + arguments[1];
+	}
+	else{
+	return function(y){ return x + y;};
+	}
+
+}
+
+console.log(sum(2,3));
+console.log(sum (2)(3));
+
+// 8
+function isPalindrome(str) {
+    str = str.replace(/\W/g, '').toLowerCase(); // Removes non-word characters and lowers case
+    return (str == str.split('').reverse().join(''));
+}
+
+console.log(isPalindrome('Level'));   // true
+console.log(isPalindrome('Levels'));  // false
+console.log(isPalindrome('A man, a plan, a canal, Panama')); // true
+console.log(isPalindrome('No lemon, no melon!')); // true
+
+// 9
+const isPalindrome = s => s.toLowerCase() === [...s.toLowerCase()].reverse().join('');
+
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello"));   // false
+console.log(isPalindrome("Madam"));   // true
+
+// 10
+const isPalindrome = s => {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, ''); // Remove non-alphanumeric characters
+    return s === [...s].reverse().join('');
+};
+
+// Test Cases
+console.log(isPalindrome("racecar"));   // true
+console.log(isPalindrome("hello"));     // false
+console.log(isPalindrome("Madam"));     // true
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
+console.log(isPalindrome("No lemon, no melon!")); // true
+
+// 11
+var y = 1, x = y = typeof x;
+ 
+console.log(y);
+console.log(x);
+// Assignments in JavaScript are evaluated right to left.
+// typeof never throws an error for an undeclared variable; it returns "undefined".
+// x = y = typeof x; first assigns "undefined" to y, then assigns y to x.
+
+// 12
+var a = (2, 3, 5);
+console.log(a);
+
+// 13
+var y = 1;
+if (function f(){}) {
+y += typeof f;
+}
+console.log(y);
+
+// 13
+(function() {
+	'use strict';
+	var a = window.b = 5;
+	})();
+	console.log(b);
+
+// 14
+function test() {
+	console.log(a);
+	console.log(foo());
+	var a = 1;
+	function foo() {
+	return 2;
+	}
+	}
+	test();
