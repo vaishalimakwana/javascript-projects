28b9f5495e4475d8a0c1eeee05ac5480560e99d8
vaishalimakwana <24860123+vaishalimakwana@users.noreply.github.com>
2025-02-28 08:46:01 +0000

    DSA modified

--- DSA/index.js
+++ DSA/index.js
@@ -41,3 +41,39 @@ function firstNonRepeatedChar(str) {
     console.log(firstNonRepeatedChar("swiss"));  // Output: "w"
     console.log(firstNonRepeatedChar("racecars")); // Output: "e"
     console.log(firstNonRepeatedChar("aabbcc")); // Output: null (all are repeated)
+
+    // 3 Factorial of 5
+    function findFactorial(num){
+        let factorial = 1;
+        
+          for(let i = 1; i<= num; i++){
+             factorial *= i;
+              
+          }
+        
+        
+         console.log(factorial);
+        }
+        console.log(findFactorial(5));
+
+// 4. FizzBuzz
+function fizzBuzz(n){
+
+ const arr = [];
+
+	for(let i = 1; i<= n; i++){
+		if(i%15 === 0){
+			arr.push('FizzBuzz')
+		}
+		else if(i%3 === 0){
+			arr.push('Fizz')
+		}
+		else if(i%5 === 0){
+			arr.push('Buzz')
+		}
+		else arr.push(i.toString())
+	}
+	return arr
+}
+
+console.log(fizzBuzz(15));        
